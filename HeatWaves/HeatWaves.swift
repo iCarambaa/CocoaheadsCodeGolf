f67a94422f79185c66ff7cdd85e588e8a570c2f0
Sven Titgemeyer <s.titgemeyer@titgemeyer-it.de>
2018-05-20 12:26:42 +0200

    add explaination of the challenge.

--- HeatWaves/HeatWaves.swift
+++ HeatWaves/HeatWaves.swift
@@ -8,6 +8,25 @@
 
 import Cocoa
 
+// Background
+//
+// The Royal Netherlands Meteorological Institute defines a heat wave* as a
+// series of at least 5 consecutive days of ≥25°C weather (“summery weather”),
+// such that at least 3 of those days are ≥30°C (“tropical weather”).
+//
+// The tropical weather doesn't have to be measured consecutively: for example:
+// 30, 25, 30, 26, 27, 28, 32, 30 is a 8-day long heat wave with 4 days of
+// tropical weather.
+//
+// *(Well, by Dutch standards.)
+//
+// Challenge
+//
+// Given a non-empty list of positive integers representing Celsius temperature
+// measurements from successive days, decide whether that list contains a heat
+// wave (as per the above definition).
+//
+// The shortest answer in bytes wins.
 public func isHeatWaveIncludedIn(waves: [Int]) -> Bool {
     return false
 }
