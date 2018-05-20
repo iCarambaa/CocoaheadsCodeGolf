e5c21fb590e0a0216c4e6725ea5b720d03ed4465
Sven Titgemeyer <s.titgemeyer@titgemeyer-it.de>
2018-05-20 12:23:23 +0200

    Add test cases.

---
+++ HeatWaves/HeatWaves.swift
@@ -0,0 +1,13 @@
+//
+//  HeatWaves.swift
+//  HeatWaves
+//
+//  Created by Sven Titgemeyer on 20.05.18.
+//  Copyright © 2018 Cocoaheads Aachen. All rights reserved.
+//
+
+import Cocoa
+
+public func isHeatWaveIncludedIn(waves: [Int]) -> Bool {
+    return false
+}
